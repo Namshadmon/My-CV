e614cbadde32957c0cada772e67e5f7833685249
Namshad <namshadnpt@gmail.com>
2021-05-20 01:18:53 +0530

    sds

--- Updated/js/scripts.js
+++ Updated/js/scripts.js
@@ -392,7 +392,7 @@ $(function () {
 		success: "valid",
 		submitHandler: function () {
 			$.ajax({
-				url: '../mailer/feedback.php',
+				url: '/mailer/feedback.php',
 				type: 'post',
 				dataType: 'json',
 				data: 'name=' + $("#cform").find('input[name="name"]').val() + '&email=' + $("#cform").find('input[name="email"]').val() + '&message=' + $("#cform").find('textarea[name="message"]').val(),
