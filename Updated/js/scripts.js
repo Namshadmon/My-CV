a6acec700388d52fd60a04f3339ada4bd4c59374
Namshad <namshadnpt@gmail.com>
2021-05-20 01:53:31 +0530

    js

--- Updated/js/scripts.js
+++ Updated/js/scripts.js
@@ -361,6 +361,7 @@ $(function () {
 	var getUrl = window.location;
 	var baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
 
+	var contextPath = "${pageContext.request.contextPath}";
 
 	/*
 		Validate Contact Form
@@ -392,10 +393,9 @@ $(function () {
 		success: "valid",
 		submitHandler: function () {
 			$.ajax({
-				url: 'mailer/feedback.php',
+				url: "${pageContext.request.contextPath}/mailer/feedback.php",
 				type: 'post',
 				dataType: 'json',
-				headers: { "Content-Type": "multipart/form-data" },
 				data: 'name=' + $("#cform").find('input[name="name"]').val() + '&email=' + $("#cform").find('input[name="email"]').val() + '&message=' + $("#cform").find('textarea[name="message"]').val(),
 				beforeSend: function () {
 
