047ca956ba57de2b905963b1d14c6d99944a38b5
Namshad <namshadnpt@gmail.com>
2021-05-19 23:15:05 +0530

    Mail Added

--- Updated/mailer/form_setting.php
+++ Updated/mailer/form_setting.php
@@ -1,8 +1,8 @@
 <?php
 	/*Form settings*/
-	$subj = "New message from the site 'vCard & Resume Template'"; //letter subject
-	$to = 'npt@gmail.com'; // Enter Your E-mail
-	$from = 'admin@you-site-name.com'; // Admin e-mail
-	$fromName = 'Your Company Name'; // Your company name
+	$subj = "New message from the site 'namshad.me'"; //letter subject
+	$to = 'namshadnpt@gmail.com'; // Enter Your E-mail
+	$from = 'namshad.pulakkal@gmail.com'; // Admin e-mail
+	$fromName = 'Namshad Portfolio'; // Your company name
 	$charset = 'UTF-8';
 ?>
